e3570a3b7558bd2f9b31ed2fc89193bdf9ebdc52
Noman6542 <mdaanoman6@gmail.com>
2025-12-08 02:13:43 +0600

    added mapcontainer

--- src/Page/Decorator/Decorator.jsx
+++ src/Page/Decorator/Decorator.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 const Decorator = () => {
   return (
-    <div className='max-w-6xl mx-auto'>
-      hi iam a Decorator
+    <div className="max-w-6xl mx-auto">
+      <h2 className="text-5xl">We are available in 64 district</h2>
+      {/* something will be happen */}
+      <div></div>
+
+      {/* main thing of map */}
+
+      <div>
+        <MapContainer>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </div>
     </div>
   );
 };
